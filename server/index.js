2772c3c4c968ddc5acac3d6f32e269740c91faf0
Dilip Singh Naruka <dsnaruka995082@gmail.com>
2023-05-18 08:37:58 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,43 @@
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const socketIo = require("socket.io");
+
+const app = express();
+const port = 4500 || process.env.PORT;
+
+const users = [{}];
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("HELL ITS GOING ON ");
+});
+
+const server = http.createServer(app);
+const io = socketIo(server);
+
+server.listen(port, () => {
+  console.log(`server is working on ${port}`);
+});
+
+io.on("connection", (socket) => {
+  // console.log("New Connection");
+  socket.on("joined", ({ user }) => {
+        users[socket.id] = user;
+        // console.log(`${user} has joined`);
+        socket.broadcast.emit("userJoined", { user: "Admin", message: `${users[socket.id]} Usersssss has joined`,});
+        socket.emit(`welcome`, {user: "Admin", message: `welcome to the chat, ${users[socket.id]}`,
+    });
+    socket.on('message',({message,id})=>{
+        io.emit(`sendMessage`, {user:users[id],message,id})
+    })
+    socket.on("disconnect", () => {
+      socket.broadcast.emit("leave", {
+        user: "Admin",
+        message: `${users[socket.id]} has left`,
+      });
+      // console.log(`${users[socket.id]} user left`);
+    });
+  });
+});
